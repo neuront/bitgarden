8492cc55b5d018e3492c88799c9b5efdca70b5a0
Neuron Teckid <Lene13@gmail.com>
2013-03-27 17:53:11 +0800

    change asynchronous styles

--- pymongo-demo/handlers.py
+++ pymongo-demo/handlers.py
@@ -21,67 +21,90 @@ class BaseHandler(tornado.web.RequestHandler):
         self.set_header('Content-Type', 'application/json')
         self.write(json.dumps(obj))
 
-class Index(BaseHandler):
+class AsyncHandler:
+    @tornado.web.asynchronous
+    def serve(self):
+        def callback(result, error):
+            if error:
+                raise tornado.web.HTTPError(500)
+            if self.on_result(result) is None:
+                self.finish()
+        self.async_call(callback, **self.query_args())
+
+class AsyncGetHandler(BaseHandler, AsyncHandler):
     @tornado.web.asynchronous
     def get(self):
-        models.first_page(self.on_result)
+        self.serve()
 
-    def on_result(self, result, error):
-        if error:
-            raise tornado.web.HTTPError(500)
+class AsyncPostHandler(BaseHandler, AsyncHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        self.serve()
+
+class Index(AsyncGetHandler):
+    def initialize(self):
+        self.async_call = models.first_page
+
+    def query_args(self):
+        return dict()
+
+    def on_result(self, result):
         self.write(render('index.html', topics=result))
-        self.finish()
 
-class SingleTopic(BaseHandler):
-    @tornado.web.asynchronous
-    def get(self):
-        models.topic_with_comments(self.arg('id'), self.on_result)
+class SingleTopic(AsyncGetHandler):
+    def initialize(self):
+        self.async_call = models.topic_with_comments
 
-    def on_result(self, result, error):
-        if error:
-            raise tornado.web.HTTPError(500)
+    def query_args(self):
+        return { 'topic_id': self.arg('id') }
+
+    def on_result(self, result):
         if result:
             self.write(render('topic.html', topic=result))
         else:
             self.set_status(404)
             self.write(render('not-found.html'))
-        self.finish()
 
-class Search(BaseHandler):
-    @tornado.web.asynchronous
-    def get(self):
+class Search(AsyncGetHandler):
+    def initialize(self):
+        self.async_call = models.query
+
+    def query_args(self):
         tags = self.arg('tags')
         if not tags:
             tags = ''
         self.tags = filter(lambda s: len(s) > 0, tags.split(','))
-        models.query({ 'title': self.arg('title'), 'tags': self.tags },
-                     self.on_result)
+        return { 'title': self.arg('title'), 'tags': self.tags }
 
-    def on_result(self, result, error):
-        if error:
-            raise tornado.web.HTTPError(500)
+    def on_result(self, result):
         self.write(render('search-result.html', topics=result, tags=self.tags))
-        self.finish()
 
-class AddTopic(BaseHandler):
-    @tornado.web.asynchronous
-    def post(self):
-        models.add_topic(self.arg('title'),
-                         [ t.strip() for t in self.arg('tags').split(',') ],
-                         self.arg('content'), self.on_result)
+class AddTopic(AsyncPostHandler):
+    def initialize(self):
+        self.async_call = models.add_topic
 
-    def on_result(self, result, error):
-        if error:
-            raise tornado.web.HTTPError(500)
+    def query_args(self):
+        return {
+                'title': self.arg('title'),
+                'tags': [ t.strip() for t in self.arg('tags').split(',') ],
+                'content': self.arg('content'),
+            }
+
+    def on_result(self, result):
         self.redirect('/')
+        return True
 
-class AddComment(BaseHandler):
-    @tornado.web.asynchronous
-    def post(self):
+class AddComment(AsyncPostHandler):
+    def initialize(self):
+        self.async_call = models.add_comment
+
+    def query_args(self):
         self.topic_id = self.arg('topic_id')
-        models.add_comment(self.topic_id, self.arg('content'), self.on_result)
+        return {
+                'topic_id': self.topic_id,
+                'content': self.arg('content'),
+            }
 
-    def on_result(self, result, error):
-        if error:
-            raise tornado.web.HTTPError(500)
+    def on_result(self, result):
         self.redirect('/topic?id=' + self.topic_id)
+        return True
--- pymongo-demo/models.py
+++ pymongo-demo/models.py
@@ -6,6 +6,24 @@ import bson.dbref
 
 import appenv
 
+def asynchronous(callback):
+    def wrapper(f):
+        def g(result, error):
+            if error:
+                return callback(None, error)
+            f(result)
+        return g
+    return wrapper
+
+def ensynchronous(callback):
+    def wrapper(f):
+        def g(result, error):
+            if error:
+                return callback(None, error)
+            return callback(f(result), None)
+        return g
+    return wrapper
+
 db = asyncmongo.Client(pool_id='mongodemo', dbname='mongodemo',
                        host='localhost', port=appenv.env().db_port)
 
@@ -14,7 +32,7 @@ COUNT_PER_PAGE = 16
 def ref(collection, obj):
     return bson.dbref.DBRef(collection, str(obj['_id']))
 
-def add_topic(title, tags, content, cb):
+def add_topic(cb, title, tags, content):
     db.topics.insert({
             'title': title,
             'tags': tags,
@@ -25,33 +43,29 @@ def add_topic(title, tags, content, cb):
 def topic_only(uid, cb):
     db.topics.find_one({'_id': bson.objectid.ObjectId(uid)}, callback=cb)
 
-def query(criterion, cb):
+def query(cb, title, tags):
     cri = dict()
-    if criterion['title']:
-        cri['title'] = criterion['title']
-    if len(criterion['tags']) > 0:
-        cri['tags'] = { '$all' : criterion['tags'] }
+    if title: cri['title'] = title
+    if len(tags) > 0: cri['tags'] = { '$all' : tags }
     db.topics.find(cri, callback=cb)
 
 def first_page(cb):
     db.topics.find({}, limit=COUNT_PER_PAGE, callback=cb)
 
-def add_comment(tid, content, cb):
+def add_comment(cb, topic_id, content):
     db.comments.insert({
-            'topic': bson.dbref.DBRef('topics', tid),
+            'topic': bson.dbref.DBRef('topics', topic_id),
             'content': content,
             'post_time': datetime.datetime.utcnow(),
         }, callback=cb)
 
-def topic_with_comments(tid, cb):
-    def load_comments(topic, error):
-        def return_topic(comments, error):
-            if error:
-                return cb(None, error)
+def topic_with_comments(cb, topic_id):
+    @asynchronous(cb)
+    def load_comments(topic):
+        @ensynchronous(cb)
+        def return_topic(comments):
             topic['comments'] = comments
-            cb(topic, None)
-        if error:
-            return cb(None, error)
+            return topic
         db.comments.find({ 'topic': ref('topics', topic) },
                          callback=return_topic)
-    topic_only(tid, load_comments)
+    topic_only(topic_id, load_comments)
